e7f60316cd7b52f8cd35f43bc4dee111b519ae8d
SultanRayhan32 <45747249+SultanRayhan32@users.noreply.github.com>
2019-05-05 13:04:15 +0700

    Update exercise-15.js

--- exercise-15.js
+++ exercise-15.js
@@ -1,28 +1,41 @@
 function groupAnimals(animals) {
-    var temp = [];
-
-    for(var x = 0; x < animals.length -1; x++){
-        for(var y = x + 1; y < animals.length ; y++){ 
-            if(animals[x][0] === animals[y][0]){
-                temp.push([animals[x],animals[y]])
-                animals[x] += '#';
-                animals[y] += '#';
+    
+    var tampungHuruf = []
+    var tampungHuruf2 = [];
+    
+    for(var x = 0; x < animals.length; x++){
+        tampungHuruf.push([animals[x]])
+    } 
+    for(var x = 0; x < tampungHuruf.length; x++){
+        for(var i = x + 1; i < tampungHuruf.length; i++){
+            if(tampungHuruf[x][0][0] === tampungHuruf[i][0][0]){
+                tampungHuruf[x].push(tampungHuruf[i][0])
+                tampungHuruf[i][0] = ''
+            }
+        }
+    } 
+    for(var x = 0; x < tampungHuruf.length;x++){
+        if(tampungHuruf[x][0] !== ''){
+            tampungHuruf2.push(tampungHuruf[x])
+        }
+    } 
+    var temp = ''
+    for(var x = 0; x < tampungHuruf2.length; x++){
+        for(var i = x + 1 ; i < tampungHuruf2.length; i++){
+            if(tampungHuruf2[x][0] > tampungHuruf2[i][0]){
+                temp = tampungHuruf2[x]
+                tampungHuruf2[x] = tampungHuruf2[i]
+                tampungHuruf2[i] = temp
             }
         }
     }
 
-    for(var x in animals){
-        if(animals[x][animals[x].length-1] !== '#'){
-            temp.push([animals[x]])
-        }
-    }
+    return tampungHuruf2
 
-    temp.sort()
-    return temp
 }
   
   // TEST CASES
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil'   ]));
   // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
   console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
   // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
